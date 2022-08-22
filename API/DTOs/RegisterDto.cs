5a997c07d11986facd54255f6045e40e33002a38
Learnmore <chimedzalearnmore@gmail.com>
2022-08-22 10:21:27 +0200

    End of Section 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -10,6 +10,11 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
+        [Required] public string KnownAs { get; set; }
+        [Required] public string Gender { get; set; }
+        [Required] public DateTime DateOfBirth { get; set; }
+        [Required] public string City { get; set; }
+        [Required] public string Country { get; set; }
         [Required]
         [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and a maximun length of 8.")]
         public string Password { get; set; }
